9f90a3c1219f733e1f1e5ac7b883482e192157f2
Orko15 <50932841+Orko15@users.noreply.github.com>
2023-02-02 23:19:42 -0700

    off initial algo update

--- Gaia_knock_off/off_road.py
+++ Gaia_knock_off/off_road.py
@@ -5,9 +5,17 @@ import requests
 set up roads API infastructure
 
 find patch of area defined by, nearest road being 10km away from nearest road within
-distance start_distance to end distance radius
+distance start_distance to end distance radius given by the user
 
-find 
+find list of roads that border the area, 
+
+find road closet to starting point
+find road closet to ending point
+get 2 on road directions
+
+link to the via patch
+
+off road instructions 
 
 '''
 
@@ -21,12 +29,23 @@ class nearest_roads_api(object):
         headers: dictionary
         api_key: google directions api key
 
-
-
         :return
         '''
         self.pointers = pointers
         self.payload = payload
         self.headers = headers
         self.api_key = api_key
+
+    def pointers_string(self):
+
+        # url = "https://roads.googleapis.com/v1/nearestRoads?points=60.170880%2C24.942795%7C60.170879%2C24.942796%7C60.170877%2C24.942796&key=YOUR_API_KEY"
+
+        base_url = "https://roads.googleapis.com/v1/nearestRoads?"
+
+        pointers = self.pointers
+
+        pointer_string = "points="
+        for lat, lng in pointers:
+
+            pointer_string =
 
